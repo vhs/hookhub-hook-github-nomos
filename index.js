c80c6b9422ce980a2b250b12c7078294e183c7ab
Ty Eggen <tycho.eggen@gmail.com>
2018-09-30 21:02:25 -0700

    Added sanity checks

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const xHubSignatureMiddleware = require('x-hub-signature').middleware;
 const smb = require('slack-message-builder');
 var rp = require('request-promise');
 
+// Perform sanity check
+router.use(function (req, res, next) {
+  if (req.header('X-Hub-Signature') == undefined || req.header('X-Hub-Signature').length < 40 || req.header('X-GitHub-Event') == undefined || req.header('X-GitHub-Event') == '' || req.rawBody == undefined) {
+    res.status(412).send({
+      result: "ERROR",
+      message: "Missing or invalid request arguments"
+    });
+  } else {
+    next();
+  }
+});
+
 // Check X-Hub-Signature
 router.use(xHubSignatureMiddleware({
   algorithm: 'sha1',
@@ -23,7 +35,7 @@ router.use(xHubSignatureMiddleware({
 router.use('/', function (req, res, next) {
   debug("Handling default request");
 
-  let post_body = generateMessage(req.header('X-GitHub-Event'),req.body);
+  let post_body = generateMessage(req.header('X-GitHub-Event'), req.body);
 
   debug("post_body:", post_body);
 
@@ -63,16 +75,16 @@ var generateMessage = function (event_type, payload) {
         slack_message = slack_message
           .text("The following commit(s) got pushed to '" + payload.repository.name + "':\r\r")
           .attachment()
-            .fallback("Required plain-text summary of the attachment.")
-            .color("#0000cc")
-            .authorName(payload.sender.login)
-            .authorLink(payload.sender.html_url)
-            .authorIcon(payload.sender.avatar_url)
-            .title("Commit: " + commit.id)
-            .titleLink(commit.url)
-            .text(commit.message)
-            .footer("Via: vhs-hookhub-github-nomos")
-            .ts(Math.round(Date.parse(commit.timestamp) / 1000))
+          .fallback("Required plain-text summary of the attachment.")
+          .color("#0000cc")
+          .authorName(payload.sender.login)
+          .authorLink(payload.sender.html_url)
+          .authorIcon(payload.sender.avatar_url)
+          .title("Commit: " + commit.id)
+          .titleLink(commit.url)
+          .text(commit.message)
+          .footer("Via: vhs-hookhub-github-nomos")
+          .ts(Math.round(Date.parse(commit.timestamp) / 1000))
           .end();
       });
       break;
